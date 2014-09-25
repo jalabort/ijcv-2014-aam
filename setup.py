277c4b12861f7692c82ead468fb73599862994a5
Joan Alabort-i-Medina <ja310@imperial.ac.uk>
2014-09-25 19:26:59 +0100

    Adds simple setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+project_name = 'ijcv_2004_aam'
+
+requirements = ['menpo>=0.3.0']
+
+setup(name=project_name,
+      version='0.0',
+      description='AAM repository',
+      author='Joan Alabort-i-Medina',
+      author_email='joan.alabort@gmail.com',
+      packages=find_packages(),
+      install_requires=requirements)
